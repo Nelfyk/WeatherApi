134c9436dcb0132b30c03d680d9ae835e786be59
Nelf <ruslan-burduzhan@mail.ru>
2022-09-07 17:51:16 +0500

    create project

---
+++ src/WeatherApi.java
@@ -0,0 +1,9 @@
+public class WeatherApi {
+    public static void main(String[] args) {
+        // 🍺
+        // API Key: 1d30c485d96c4273a8d92635220709
+        // Docs: https://www.weatherapi.com/docs/
+
+
+    }
+}
